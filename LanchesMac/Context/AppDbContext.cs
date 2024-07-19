63237dd590a9649f1423275090c44d1d883b652e
Antonio Barbosa <acrb1108@gmail.com>
2024-07-19 19:15:44 -0300

    Add project files.

---
+++ LanchesMac/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using LanchesMac.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Context;
+
+public class AppDbContext : DbContext
+{
+    //A classe DbContextOptions, define as opções a serem usadas pelo DbContext.
+    //e ela vai carregar as informações de configurações necessárias para configurar o DbContext
+    //então não vamos precisar definir nada, pois será feito pelo construtor do DbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    //Agora vamos definir as propriedades DbSet, ela vai definir quais classes eu quero mapear para quais tabelas
+    //O nome das propriedades de cada DbSet "Categorias" e "Lanches", define o nome da tabela que o EntityFrameworkCore, vai criar
+    public DbSet<Categoria> Categorias { get; set; }
+    public DbSet<Lanche> Lanches { get; set; }
+
+}
---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace LanchesMac.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
+
+    //Definir o relacionamento de 1 para muitos
+    //Uma categoria possui muitos lanches
+    public List<Lanche> Lanches { get; set; }
+}
