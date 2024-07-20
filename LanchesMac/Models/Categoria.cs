b75fee15a6fd5a2979247d4b85cf94e02ab6d358
Antonio Barbosa <acrb1108@gmail.com>
2024-07-20 20:36:59 -0300

    Atualizações

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,29 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿namespace LanchesMac.Models;
 
-namespace LanchesMac.Models
+public class Categoria
 {
-    [Table("Categorias")]
-    public class Categoria
-    {
-        [Key]
-        public int CategoriaId { get; set; }
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
 
-        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
-        [Required(ErrorMessage = "Informe o nome da categoria")]
-        [Display(Name = "Nome")]
-        public string CategoriaNome { get; set; }
-
-
-        //Definir o relacionamento de 1 para muitos
-        //Uma categoria possui muitos lanches
-
-        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
-        [Required(ErrorMessage = "Informe a descrição da categoria")]
-        [Display(Name = "Descrição")]
-        public string Descricao { get; set; }
-
-        public List<Lanche> Lanches { get; set; }
-
-    }
+    //Definir o relacionamento de 1 para muitos
+    //Uma categoria possui muitos lanches
+    public List<Lanche> Lanches { get; set; }
 }
