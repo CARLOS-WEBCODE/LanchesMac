510c8f5aaa4f38929dc02c660c21bea5a24748e9
Antonio Barbosa <acrb1108@gmail.com>
2024-07-20 22:50:18 -0300

    Incluindo dados no banco, usando migration.

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,12 +1,23 @@
-﻿namespace LanchesMac.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
+namespace LanchesMac.Models;
+
+[Table("Categorias")]
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
+
+    [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+    [Required(ErrorMessage = "Informe o nome da categoria")]
+    [Display(Name = "Nome")]
     public string CategoriaNome { get; set; }
+
+    [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+    [Required(ErrorMessage = "Informe a descrição da categoria")]
+    [Display(Name = "Descrição")]
     public string Descricao { get; set; }
 
-    //Definir o relacionamento de 1 para muitos
-    //Uma categoria possui muitos lanches
     public List<Lanche> Lanches { get; set; }
 }
