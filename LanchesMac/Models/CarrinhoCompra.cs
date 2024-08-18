2afb141b74b00074dc19bd88dfa06aba34a98bc7
Antonio Barbosa <acrb1108@gmail.com>
2024-08-18 19:20:55 -0300

    Criando o Controlador para gerenciar os itens do carrinho de compras.

--- LanchesMac/Models/CarrinhoCompra.cs
+++ LanchesMac/Models/CarrinhoCompra.cs
@@ -99,4 +99,13 @@ public class CarrinhoCompra
         _context.CarrinhoCompraItens.RemoveRange(carrinhoItens);
         _context.SaveChanges();
     }
+
+    public decimal GetCarrinhoCompraTotal()
+    {
+        var total = _context.CarrinhoCompraItens
+            .Where(c => c.CarrinhoCompraId == CarrinhoCompraId)
+            .Select(c => c.Lanche.Preco * c.Quantidade).Sum();
+        
+        return total;
+    }
 }
