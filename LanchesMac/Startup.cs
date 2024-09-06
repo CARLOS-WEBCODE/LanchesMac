ea23353712ad62a2b15ef0bc2f79aac215805f5c
Antonio Barbosa <acrb1108@gmail.com>
2024-09-06 00:13:25 -0300

    Apresentando o conceito de Areas - Criando a área Admin

--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -74,6 +74,14 @@ public class Startup
 
         app.UseEndpoints(endpoints =>
         {
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
             endpoints.MapControllerRoute(
                 name: "categoriaFiltro",
                 pattern: "Lanche/{action}/{categoria?}",
