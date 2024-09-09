acceba5f26d1f791bf9380c0a17fd82c69414474
Antonio Barbosa <acrb1108@gmail.com>
2024-09-08 23:54:31 -0300

    Definindo o perfil para o usuário Admin

---
+++ LanchesMac/Services/SeedUserRoleInitial.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LanchesMac.Services;
+
+public class SeedUserRoleInitial : ISeedUserRoleInitial
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public SeedUserRoleInitial(UserManager<IdentityUser> userManager, 
+        RoleManager<IdentityRole> roleManager)
+    {
+        _userManager = userManager;
+        _roleManager = roleManager;
+    }
+
+    public void SeedRoles()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SeedUsers()
+    {
+        throw new NotImplementedException();
+    }
+}
