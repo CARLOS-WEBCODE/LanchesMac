76152131e9e67574c5ee627bbc715f7154dfc97b
Antonio Barbosa <acrb1108@gmail.com>
2024-09-08 23:59:35 -0300

    Atualização

--- LanchesMac/Services/SeedUserRoleInitial.cs
+++ LanchesMac/Services/SeedUserRoleInitial.cs
@@ -16,7 +16,21 @@ public class SeedUserRoleInitial : ISeedUserRoleInitial
 
     public void SeedRoles()
     {
-        throw new NotImplementedException();
+        if (!_roleManager.RoleExistsAsync("Member").Result)
+        {
+            IdentityRole role = new IdentityRole();
+            role.Name = "Member";
+            role.NormalizedName = "MEMBER";
+            IdentityResult roleResult = _roleManager.CreateAsync(role).Result;
+        }
+
+        if (!_roleManager.RoleExistsAsync("Admin").Result)
+        {
+            IdentityRole role = new IdentityRole();
+            role.Name = "Admin";
+            role.NormalizedName = "ADMIN";
+            IdentityResult roleResult = _roleManager.CreateAsync(role).Result;
+        }
     }
 
     public void SeedUsers()
